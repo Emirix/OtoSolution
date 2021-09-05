c82f4bf19f93b6a1ef2adab16176b4e31f6def87
Emir Tanır <emirtanir55@gmail.com>
2021-09-05 14:39:08 +0300

    dropdown search

--- src/views/components/Dropdown.jsx
+++ src/views/components/Dropdown.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Buyutec from "../../assets/svg/buyutec.svg"
 function Dropdown({title,data,object,index,onChange,onSelect}) {
+    const [payload,setPayload] = useState(data)
     return (
         <div className="emir-dropdown">
             <div className="d-flex justify-content-between align-items-center mb-3">
             <div className="title">Select {title}</div>
             <div className="dp-search">
                 <img src={Buyutec} width="20" height="20" />
-                <input onChange={e=>{onChange(e)}} type="text" placeholder={"Search "+ title} />
+                <input onChange={e=>{
+                    onChange(e)
+                    
+                }}
+                
+                onKeyUp={e=>{
+                    var filter, li, a, i, txtValue;
+                    filter = e.target.value.toUpperCase();
+                    li = document.querySelectorAll(".emir-dropdown__select");
+
+                    for (i = 0; i < li.length; i++) {
+                        a = li[i]
+                        txtValue = a.textContent || a.innerText;
+                        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                            li[i].style.display = "";
+                        } else {
+                            li[i].style.display = "none";
+                        }
+                    }
+                }}
+                
+                type="text" placeholder={"Search "+ title} />
             </div>
             </div>
             <div className="emir-dropdown__selectbox">
