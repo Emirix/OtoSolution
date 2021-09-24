67ba0a88aef6b4e8afdff63cd44c8deed3dc7598
Emir Tanır <emirtanir55@gmail.com>
2021-09-24 23:15:59 +0300

    24 eylul2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from "axios"
 import App from './App';
-axios.defaults.baseURL = 'http://34.67.64.2/';
-//axios.defaults.baseURL = 'https://api2.managedautos.com';
+//axios.defaults.baseURL = 'http://34.67.64.2/';
+axios.defaults.baseURL = 'https://api2.managedautos.com';
 
 
 
